d55c6ed88395ebdb8c149325de513afcc9793621
Paras Dattera <parasdattera555@gmail.com>
2023-12-02 22:53:56 +0530

    general commit

--- JAVASCRIPT ADVANCE TOPICS/JAVASCRIPT ASYNCHRONOUS/JAVASCRIPT ASYNCHRONOUS.js
+++ JAVASCRIPT ADVANCE TOPICS/JAVASCRIPT ASYNCHRONOUS/JAVASCRIPT ASYNCHRONOUS.js
@@ -3,5 +3,23 @@ asynchronous operations or asynchronous programming --
 
 asynchronous programming allows a program to work on multiple tasks simultaneously instead of completing one task before moving on to the next one. This can make the program get more things done in a shorter amount of time.
 
+Async-Await , setTimeout()  keywords are used while handling asynchronous programming which is advance of promise
+
 
 */
+
+
+// sample synchronous code
+
+console.log(1)
+console.log(2)
+console.log(3)
+
+// sample asynchronous code
+
+// using setTimeout method
+console.log(1);
+setTimeout(() => {
+    console.log(2);
+}, 1000);
+console.log(3);
--- JAVASCRIPT ADVANCE TOPICS/JAVASCRIPT PROMISE/JAVASCRIPT PROMISE.js
+++ JAVASCRIPT ADVANCE TOPICS/JAVASCRIPT PROMISE/JAVASCRIPT PROMISE.js
@@ -43,23 +43,104 @@ The finally() method gets executed when the promise is either resolved successfu
 // sample -- creating promise
 
 
-const Ticket = new Promise(function (resolve,reject){
-    const isBoarded = true;
-    if(isBoarded){
-        resolve("You are  in the flight .")
-    }else{
-        reject("Your flight has been cancelled")
-    }
-})
+// const Ticket = new Promise(function (resolve,reject){
+//     const isBoarded = true;
+//     if(isBoarded){
+//         resolve("You are  in the flight .")
+//     }else{
+//         reject("Your flight has been cancelled")
+//     }
+// })
 
 // using promise
 
-Ticket.then((data)=>{
-    console.log('wohoo',data);
-}).catch((data)=>{
-    console.log("oh no",data)
-}).finally(()=>{
-    console.log("thanks.")
-});
+// Ticket.then((data)=>{
+//     console.log('wohoo',data);
+// }).catch((data)=>{
+//     console.log("oh no",data)
+// }).finally(()=>{
+//     console.log("thanks.")
+// });
 
 // this above promise is synchronous in nature
+
+// to handle the asynachronous programming concepts advance version of promise which is async-await is introduced later
+
+
+
+// some samples of promise
+
+// let complete = false;
+
+// let prom = new Promise(function(resolve,reject){
+//     if(complete){
+//         resolve("I am successful.")
+//     }else{
+//         reject("I am failed.")
+//     }
+// });
+
+// console.log(prom);
+
+// parameter value inside function
+
+// function prom(complete){
+//     return new Promise(function(resolve,reject){
+//         console.log("Fetching data wait..")
+//         setTimeout(() => {
+//             if(complete){
+//                 resolve("I am successful.")
+//             }else{
+//                 reject("I am failed.")
+//             }
+//         }, 1000);
+//     });
+
+// }
+
+
+// using above promise by then and catch
+
+// let onfulfilment = (result)=>{
+//     console.log(result);
+// }
+// let onrejection = (error)=>{
+//     console.log(error);
+// }
+
+// prom(false).then(onfulfilment).catch(onrejection)
+
+// also we can create direct function inside in then and catch
+
+// prom(true).then((result)=>{
+//     console.log(result);
+// }).catch((error)=>{
+//     console.log(error);
+//      });
+
+
+
+
+// real life sample of using promise division btw two numbers
+
+function prom(a,b){
+    return new Promise(function(resolve,reject){
+        console.log("Fetching data wait..")
+        var c= a/b;
+        setTimeout(() => {
+            if(a,b){
+                resolve(`your answer is : ${c}`)
+            }else{
+                reject("failed to calculate.")
+            }
+        }, 2000);
+    });
+
+}
+
+
+prom(9,2).then((result)=>{
+    console.log(result);
+}).catch((error)=>{
+    console.log(error);
+     });
