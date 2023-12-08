ed20d2e83537729c530462a23c95e233af934bb5
Paras Dattera <parasdattera555@gmail.com>
2023-12-09 03:37:57 +0530

    general commit

---
+++ JAVASCRIPT ADVANCE TOPICS/JAVASCRIPT ASYNCHRONOUS & ASYNC AWAIT/JAVASCRIPT ASYNC & AWAIT.js
@@ -0,0 +1,75 @@
+/*
+
+in promise we need to create resolve and reject function everytime we create it so thats the main reason for using async function it returns promise and we can just use then or catch for handling promise without creating resove and reject function
+
+An async function declaration creates an AsyncFunction object. Each time when an async function is called, it returns a new Promise which will be resolved with the value returned by the async function, or rejected with an exception uncaught within the async function.
+
+*/
+
+// samle -- async
+
+// async function test(){
+//     return "Test is working" ;
+// }
+
+// test().then((result)=>{
+//     console.log(result)
+
+// }).catch((error)=>{
+//     console.log(error)
+// })
+
+
+/*
+
+await method is work only with async function it is used to give asynchronous nature to async function , to use it we just need to use this in any line of code in inside of the async function , by using this when we calling that async function only the code till await method and the line where it is used will get executed after that function will be paused and return to the next line where the function was called and execute the rest of the program and once all done it will presume the paused function and continue till another await method or last code of function and it goes on and on
+
+*/
+
+
+// sample without await -- without await all code inside function will be get executed
+
+// async function test1(){
+//     console.log("1")
+//     console.log("2")
+//     console.log("3")
+//     console.log("4")
+// }
+
+// test1().then(()=>{
+//     console.log("Executtion completed.")
+// }).catch(()=>{
+//     console.log("Execution failed.")
+// })
+
+
+// sample same code with await -- with await all code inside function will be get executed based on where await is used , 
+
+
+
+async function test1(){
+    console.log("1");
+    await console.log("2");
+    console.log("3");
+    console.log("4");
+}
+
+test1();
+console.log("5");
+
+
+// real life sample -- we can use async await in real life programs while working with apis as getting data from server can take time so to cover that amount of time and processes we use async and await 
+
+
+// sample -- getting data from api/url/local file
+
+async function test2(){
+    console.log("2");
+    
+}
+
+console.log("1");
+let a = test2();
+console.log("3");
+
+console.log(a);
--- JAVASCRIPT ADVANCE TOPICS/JAVASCRIPT FETCH/FETCH BASICS.js
+++ JAVASCRIPT ADVANCE TOPICS/JAVASCRIPT FETCH/FETCH BASICS.js
@@ -35,16 +35,120 @@ fetch(file/url).then(function(response){
 
 */
 
-// sample
+// sample -- getting text file
 
-function clickevent(data){
-    
-fetch("data.txt").then((response)=>{
-    return response.text();
-}).then((data)=>{
-    
-    document.getElementById("demo").innerHTML = data
-    console.log(data)
-})
+function clickevent() {
+
+    fetch("data.txt").then((response) => {
+        return response.text();
+    }).then((data) => {
+
+        document.getElementById("demo").innerHTML = data
+        console.log(data)
+    })
 
 }
+
+
+// sample -- getting json data from website
+
+function clickevent2(){
+    fetch("https://jsonplaceholder.typicode.com/users").then((response)=>{
+        return response.json()
+    }).then((data)=>{
+        console.log(data);
+        for(var x in data){
+            document.write(`${data[x].name} - ${data[x].address.city} - ${data[x].email} <br>`)
+        }
+    })
+}
+        
+
+// sample -- getting json data from local file
+
+function clickevent3(){
+    fetch("data2.json").then((response)=>response.json()).then((data)=>{
+        console.log(data);
+        document.getElementById("demo3").innerHTML = `${data[0].name} - ${data[0].age} - ${data[0].city} <br> ${data[1].name} - ${data[1].age} - ${data[1].city} <br> ${data[2].name} - ${data[2].age} - ${data[2].city}`
+    })
+}
+
+
+// saving data on server -- updating , inserting , editing of data  -- using POST , PUT , DELETE
+
+/* syntax -- for reading data the syntax for fetch is -- fetch(url) but for inserting , updating or creating new data we need to insert the optional parameter values and the syntax for it --
+
+fetch(url,{
+    method: 'POST'/'PUT'/'DELETE,
+    body: data -- form data/ JSON data / text
+    header: {
+        'Contect-Type': 'application/json'/'application/x-www-form-urlencoded'
+    }
+})
+
+*/
+
+
+
+// sample -- inserting/creating data into json   -- POST METHOD
+
+
+fetch('https://jsonplaceholder.typicode.com/posts', {
+  method: 'POST',
+  body: JSON.stringify({ // stringify used to convert data into json data
+    title: 'foo',
+    body: 'bar',
+    userId: 1,
+  }),
+  headers: {
+    'Content-type': 'application/json; charset=UTF-8',
+  },
+})
+  .then((response) => response.json())
+  .then((json) => console.log(json));
+
+
+// sample -- updating data -- PUT METHOD
+
+fetch('https://jsonplaceholder.typicode.com/posts/1', {
+  method: 'PUT',
+  body: JSON.stringify({
+    id: 1,
+    title: 'foo',
+    body: 'bar',
+    userId: 1,
+  }),
+  headers: {
+    'Content-type': 'application/json; charset=UTF-8',
+  },
+})
+  .then((response) => response.json())
+  .then((json) => console.log(json));
+
+
+
+// for deleting data we need to just declare the method and and url of the data
+
+fetch('https://jsonplaceholder.typicode.com/posts/1', {
+  method: 'DELETE',
+});
+
+
+// sample -- getting data for inserting or updating on server by form from html
+
+
+document.getElementById("saveForm").addEventListener("click",function(e){
+    
+e.preventDefault();
+
+fetch('https://jsonplaceholder.typicode.com/posts/1', {
+  method: 'PUT',
+  body: new FormData(document.getElementById("myForm")),
+  headers: {
+    'Content-type': 'application/x-www-form-urlencoded',
+  },
+})
+  .then((response) => response.json())
+  .then((json) => console.log(json));
+
+})
